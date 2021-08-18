6fad1060812a0fcc4859672299e0564a218ae0c8
Artofaj <info@artofakume.com>
2021-08-18 08:54:21 +0200

    Addd request

--- src/pastLaunches.js
+++ src/pastLaunches.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 const PastLaunches = () => {
+
+    useEffect(() => {
+        
+        fetch('https://api.spacexdata.com/v4/launches/latest')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        return () => {
+        }
+    }, [])
 
     return (
 
