4430dedb8b00a0d1cb74563c4bdc8fc56cee7c82
Artofaj <info@artofakume.com>
2021-08-18 10:58:32 +0200

    started adding styles to components

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,8 +1,14 @@
 import React from "react";
 
+const styles= {
+  backgroundColor: "var(--primary-color)",
+  borderRadius: "var(--rounded-corners)",
+  padding: "var(--padding-sm)"
+} 
+
 const Launch = (params) => {
   return (
-    <div className="launch">
+    <div className="launch" style={styles}>
       <h1>{params.name}</h1>
       <p>#{params.id}</p>
     </div>
