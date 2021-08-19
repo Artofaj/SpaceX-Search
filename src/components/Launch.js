349b8211e053642361a21b57f83d330349800ed3
Artofaj <info@artofakume.com>
2021-08-19 11:52:35 +0200

    Nices styles

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,26 +1,22 @@
 import React from "react";
 
-
 const Launch = (params) => {
-
   const styles = {
     backgroundColor: params.success
       ? "var(--success-color)"
       : "var(--failure-color)",
     borderRadius: "var(--rounded-corners)",
     padding: "var(--padding-sm)",
-    flex: "1 1 25%"
-  }; 
-
+    flex: "1 1 25%",
+  };
 
   return (
     <div className="launch" style={styles}>
       <h1>{params.name}</h1>
-      <p>#{params.id}</p>
-
-      {!params.simple &&
-      <p>time since launch</p>
-      }
+      <div style={{justifyContent:"space-between"}} className="wrapper">
+        {!params.simple && <p>time since launch</p>}
+        <p>#{params.id}</p>
+      </div>
     </div>
   );
 };
--- src/components/searchResult.js
+++ src/components/searchResult.js
@@ -1,14 +1,14 @@
-import React from "react"
+import React from "react";
 import Launch from "./Launch";
+
 const SearchResult = () => {
+  return (
+    <section>
+      <h1>Search Result</h1>
 
-    return (
-      <section>
-        <h1>Search Result</h1>
+      <Launch key={564564564} name={"Launch1"} id={564564564} />
+    </section>
+  );
+};
 
-        <Launch key={564564564} name={"Launch1"} id={564564564} />
-      </section>
-    );
-}
-
-export default SearchResult
+export default SearchResult;
