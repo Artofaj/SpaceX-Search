34fc87991892441bc33676103193b1b7a0ae96f2
Artofaj <info@artofakume.com>
2021-08-19 16:55:33 +0200

    IMplemented counter

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
 
 const Launch = (params) => {
   const styles = {
@@ -10,13 +11,41 @@ const Launch = (params) => {
     flex: "1 1 25%",
   };
 
-  const date = params.date
+  const [now, setNow] = useState();
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => setNow(moment().diff(params.date, "seconds")),
+      1000
+    );
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+
+  const getLastDigits = (number) =>
+    number.toString().slice(number.toString().length - 2);
+
+  const getElapsedTime = () => {
+    let hours = moment().diff(params.date, "hours");
+    let minutes = getLastDigits(moment().diff(params.date, "minutes"));
+    let seconds = getLastDigits(
+      moment().diff(params.date, "seconds") - minutes * 60
+    );
+
+    return (
+      <div>
+        {hours} : {minutes} : {seconds}
+      </div>
+    );
+  };
 
   return (
     <div className="launch" style={styles}>
       <h1>{params.name}</h1>
       <div style={{ justifyContent: "space-between" }} className="wrapper">
-        {!params.simple && <p>time since launch {date}</p>}
+        {!params.simple && <div>Time elapsed since launch {getElapsedTime()} </div>}
         <p>#{params.id}</p>
       </div>
     </div>
