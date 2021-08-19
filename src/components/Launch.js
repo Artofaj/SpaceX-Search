4134042ec7f9ace1f3b52dbb818e0b856e6a1fb4
Artofaj <info@artofakume.com>
2021-08-19 17:22:34 +0200

    Now coloring only detailed Launch

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -2,17 +2,6 @@ import React, { useState, useEffect } from "react";
 import moment from "moment";
 
 const Launch = (params) => {
-  const styles = {
-    backgroundColor: params.success
-      ? "var(--success-color)"
-      : "var(--failure-color)",
-    borderRadius: "var(--rounded-corners)",
-    padding: "var(--padding-sm) var(--padding-md)",
-    flex: "1 1 25%",
-  };
-
-  const [now, setNow] = useState();
-
   useEffect(() => {
     const interval = setInterval(
       () => setNow(moment().diff(params.date, "seconds")),
@@ -22,6 +11,21 @@ const Launch = (params) => {
       clearInterval(interval);
     };
   }, []);
+
+  const [now, setNow] = useState();
+
+  const getBGColor = () => {
+    let bgc = "var(--primary-color)";
+
+    if (!params.simple && params.success) {
+      bgc = "var(--success-color)";
+    }
+    if (!params.simple && !params.success) {
+      bgc = "var(--failure-color)";
+    }
+
+    return bgc;
+  };
 
   const getLastDigits = (number) =>
     number.toString().slice(number.toString().length - 2);
@@ -49,6 +53,13 @@ const Launch = (params) => {
         {hours} <Separator /> {minutes} <Separator /> {seconds}
       </div>
     );
+  };
+
+  const styles = {
+    backgroundColor: getBGColor(),
+    borderRadius: "var(--rounded-corners)",
+    padding: "var(--padding-sm) var(--padding-md)",
+    flex: "1 1 25%",
   };
 
   return (
