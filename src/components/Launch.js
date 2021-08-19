f608984e8712084ba12ecee24466a7d535ce2665
Artofaj <info@artofakume.com>
2021-08-19 17:14:44 +0200

    Added blinking counter

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -23,20 +23,30 @@ const Launch = (params) => {
     };
   }, []);
 
-
   const getLastDigits = (number) =>
     number.toString().slice(number.toString().length - 2);
 
+  const isOdd = (num) => {
+    return num % 2;
+  };
+  const Separator = () => {
+    let blink = isOdd(now) === 1 ? true : false;
+
+    return <span style={{ opacity: blink ? 0.5 : 0.75 }}>:</span>;
+  };
+
   const getElapsedTime = () => {
     let hours = moment().diff(params.date, "hours");
-    let minutes = getLastDigits(moment().diff(params.date, "minutes"));
+    let minutes = getLastDigits(
+      moment().diff(params.date, "minutes") - hours * 60
+    );
     let seconds = getLastDigits(
       moment().diff(params.date, "seconds") - minutes * 60
     );
 
     return (
       <div>
-        {hours} : {minutes} : {seconds}
+        {hours} <Separator /> {minutes} <Separator /> {seconds}
       </div>
     );
   };
@@ -45,7 +55,9 @@ const Launch = (params) => {
     <div className="launch" style={styles}>
       <h1>{params.name}</h1>
       <div style={{ justifyContent: "space-between" }} className="wrapper">
-        {!params.simple && <div>Time elapsed since launch {getElapsedTime()} </div>}
+        {!params.simple && (
+          <div>Time elapsed since launch {getElapsedTime()} </div>
+        )}
         <p>#{params.id}</p>
       </div>
     </div>
