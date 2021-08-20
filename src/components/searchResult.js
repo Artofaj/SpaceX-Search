229ec7a7e8724f370f2e29f0b7a84299524c7ea5
Artofaj <info@artofakume.com>
2021-08-20 11:38:53 +0200

    Now getting recommendations

--- src/components/searchResult.js
+++ src/components/searchResult.js
@@ -1,11 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Launch from "./Launch";
 import { useObserver } from "mobx-react";
 import launchStore from "../stores/launchStore";
 
+const ResultError = () => {
+  const getRandom = () => {
+    let randomNumber = Math.floor(Math.random() * launchStore.launches.length);
+    return randomNumber;
+  };
+
+  const [random, setRandom] = useState(getRandom());
+
+  return (
+    <>
+      <p>Sorry, we could not find this launch, please try a valid id.</p>
+      <p>
+        How about the <u>{launchStore.launches[random].id}</u> ?
+      </p>
+      <p>
+        You can of course try{" "}
+        <button onClick={() => setRandom(getRandom())}>a different one</button>
+      </p>
+    </>
+  );
+};
+
 const SearchResult = () => {
   const getResult = () => {
-    let result = <p>Sorry, no results </p>;
+    let result = <ResultError />;
 
     if (launchStore.searchResult) {
       result = (
